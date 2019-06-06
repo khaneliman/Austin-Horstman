070ec852d3352a6aeeba78d8005206a50dee1efe
Austin Horstman <khaneliman12@gmail.com>
2019-06-05 22:40:40 -0500

    no message

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+﻿var config = {
+    apiKey: "AIzaSyBty_UDJ3uqZEtntU21iXO55W0r9fAljcw",
+    authDomain: "austin-horstman.firebaseapp.com",
+    databaseURL: "https://austin-horstman.firebaseio.com",
+    projectId: "austin-horstman",
+    storageBucket: "",
+    messagingSenderId: "1041879118826"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
