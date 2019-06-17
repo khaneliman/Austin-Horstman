923e710458e6ff06a470b2e0b94512de40db1e31
Austin Horstman <khaneliman12@gmail.com>
2019-06-16 23:27:27 -0500

    using angular cli to generate app and components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'austin-horstman';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { ResumeComponent } from './resume/resume.component';
-import { SocialLinksComponent } from './social-links/social-links.component';
-import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, AboutComponent, ResumeComponent, SocialLinksComponent, FooterComponent, HeaderComponent, ContactComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    SocialLinksComponent,
+    ResumeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
