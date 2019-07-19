e6560a82f717ea0daff96ec568297f080cf9decc
Austin Horstman <khaneliman12@gmail.com>
2019-07-18 22:12:46 -0500

    reference styling

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss',
+  '../../assets/css/_variables.scss',
+  '../../assets/css/custom.sass',
+  '../../assets/css/bootstrap.css',
+  '../../assets/css/bootstrap.min.css',
+]
 })
 export class AboutComponent implements OnInit {
 
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss',
+  '../../assets/css/_variables.scss',
+  '../../assets/css/custom.sass',
+  '../../assets/css/bootstrap.css',
+  '../../assets/css/bootstrap.min.css',
+]
 })
 export class ContactComponent implements OnInit {
 
