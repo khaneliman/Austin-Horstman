e5d28d251e877945ffca80411231bf3a82ed72fe
Austin Horstman <khaneliman12@gmail.com>
2019-07-19 19:45:23 -0500

    adding basic routing and starting to tweak resume/header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss',
+              '../assets/css/_variables.scss',
+              '../assets/css/custom.sass',
+              '../assets/css/bootstrap.css',
+              '../assets/css/bootstrap.min.css',
+            ]
 })
 export class AppComponent {
   title = 'austin-horstman';
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,7 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss',
+              '../../assets/css/_variables.scss',
+              '../../assets/css/custom.sass',
+              '../../assets/css/bootstrap.css',
+              '../../assets/css/bootstrap.min.css',
+            ]
 })
 export class HeaderComponent implements OnInit {
 
