4243271c675d8443edb991d28909122bcfbcd26d
Austin Horstman <khaneliman12@gmail.com>
2019-07-27 23:58:21 -0500

    some tweaks to routing and adding page not found game page

--- WebApp/ClientApp/src/app/app-routing.module.ts
+++ WebApp/ClientApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { SocialLinksComponent } from './social-links/social-links.component';
 import { ResumeComponent } from './resume/resume.component';
 import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './PageNotFound/PageNotFound.component';
 
 const routes: Routes = [
   { path: 'home', component: AboutComponent},
@@ -11,10 +12,8 @@ const routes: Routes = [
   { path: 'social-links', component: SocialLinksComponent},
   { path: 'resume', component: ResumeComponent},
   { path: 'contact', component: ContactComponent},
-  { path: '',
-    redirectTo: '/',
-    pathMatch: 'full'
-  }
+  { path: '', redirectTo: '/home', pathMatch: 'full'},
+  { path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -9,24 +9,39 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { SocialLinksComponent } from './social-links/social-links.component';
 import { ResumeComponent } from './resume/resume.component';
-
+import { PageNotFoundComponent } from './PageNotFound/PageNotFound.component';
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatCardModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule
+} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 @NgModule({
-   declarations: [
-      AppComponent,
-      HeaderComponent,
-      FooterComponent,
-      AboutComponent,
-      ContactComponent,
-      SocialLinksComponent,
-      ResumeComponent,
-   ],
-   imports: [
-      BrowserModule,
-      AppRoutingModule
-   ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ContactComponent,
+    SocialLinksComponent,
+    ResumeComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatProgressSpinnerModule,
+    MatButtonModule,
+    MatCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }
