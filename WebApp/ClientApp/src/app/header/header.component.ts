69c3b621ca83be3c33f499998f90f6a190280123
Austin Horstman <khaneliman12@gmail.com>
2019-08-04 09:26:20 -0500

    added activatedroute logic to show current in menu for active page

--- WebApp/ClientApp/src/app/header/header.component.ts
+++ WebApp/ClientApp/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import {ActivatedRoute, Router, NavigationEnd} from '@angular/router';
+import { map, filter, catchError, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -6,11 +8,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss'
             ]
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { }
+  public currentRoute;
 
   ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.activatedRoute),
+      map(route => {
+        while (route.firstChild) {
+         route = route.firstChild;
+        }
+        return route;
+       }),
+       map(route => {
+         console.log(route.url);
+         route.url.forEach(x => this.currentRoute = x[0].path);
+       })
+      )
+     .subscribe(val => {
+       // ... do something with the url
+       console.log('The new path : ' + this.currentRoute);
+     });
   }
 
+  ngOnChanges() {
+  }
 }
