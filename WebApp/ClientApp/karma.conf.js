b3dd235c3e6a802d1ba6981804fb2969b5aec4da
Austin Horstman <khaneliman12@gmail.com>
2021-11-02 21:08:06 -0500

    test chromium jenkins

--- WebApp/ClientApp/karma.conf.js
+++ WebApp/ClientApp/karma.conf.js
@@ -1,6 +1,7 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 process.env.CHROME_BIN = require('puppeteer').executablePath()
+process.env.CHROMIUM_BIN = require('puppeteer').executablePath()
 
 module.exports = function (config) {
   config.set({
@@ -38,7 +39,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromiumHeadless'],
     singleRun: true,
     restartOnFileChange: true
   });
