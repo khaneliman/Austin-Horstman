3606a155094ce6b23299d11e03b4a46d3c8e5fd8
Austin Horstman <khaneliman12@gmail.com>
2021-11-06 10:28:24 -0500

    Initial Dotnet Angular

---
+++ ClientApp/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { SocialLinksComponent } from './social-links/social-links.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: 'home', component: AboutComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'social-links', component: SocialLinksComponent},
+  { path: 'resume', component: ResumeComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
