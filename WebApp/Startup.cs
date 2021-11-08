aceba21bb0d046143626e6b1803a0f7393e096cb
Austin Horstman <khaneliman12@gmail.com>
2021-11-07 18:36:10 -0600

    Remove HTTPS redirection and HSTS

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -49,11 +49,8 @@ namespace austin_horstman_web_app
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
