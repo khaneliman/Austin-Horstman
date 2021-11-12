8627be23628503d8050647495142b9b05cc38067
codefactor-io <support@codefactor.io>
2021-11-12 16:44:16 +0000

    [CodeFactor] Apply fixes to commit 21f2448

--- WebApp/ClientApp/src/app/shared/shared.module.ts
+++ WebApp/ClientApp/src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// Custom components 
+// Custom components
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SocialLinksComponent } from './social-links/social-links.component';
