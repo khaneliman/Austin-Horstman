6fd1b1503c18f99726d65d66f89fa30cb955af51
Austin Horstman <khaneliman12@gmail.com>
2021-11-11 18:52:14 -0600

    move shared components into separate module

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -2,33 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { FooterComponent } from './shared/footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ResumeComponent } from './resume/resume.component';
-import { SocialLinksComponent } from './social-links/social-links.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    FooterComponent,
     AboutComponent,
     ResumeComponent,
-    SocialLinksComponent,
     ContactComponent
   ],
   imports: [
@@ -41,7 +33,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCheckboxModule,
     MatProgressSpinnerModule,
     MatToolbarModule,
-    NgbModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
