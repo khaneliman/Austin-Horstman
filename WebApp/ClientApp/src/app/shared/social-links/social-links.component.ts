cf56376a34aba5c1ed7cd68799eb39cc5607acbc
Austin Horstman <khaneliman12@gmail.com>
2021-11-11 20:23:47 -0600

    Use template syntax for social links component

--- WebApp/ClientApp/src/app/shared/social-links/social-links.component.ts
+++ WebApp/ClientApp/src/app/shared/social-links/social-links.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-social-links',
@@ -8,5 +8,45 @@ import { Component, OnInit } from '@angular/core';
 export class SocialLinksComponent {
 
   constructor() { }
+
+  @Input() location: string;
+
+  socialLinks: any = [
+    {
+      platform: 'facebook',
+      link: 'https://facebook.com/khaneliman',
+      btnClass: 'btn-facebook',
+      faClass: 'fa-facebook-square',
+      tooltip: 'Follow me on Facebook'
+    },
+    {
+      platform: 'twitter',
+      link: 'https://twitter.com/khaneliman12',
+      btnClass: 'btn-twitter',
+      faClass: 'fa-twitter',
+      tooltip: 'Follow me on Twitter'
+    },
+    {
+      platform: 'instagram',
+      link: 'https://instagram.com/khaneliman',
+      btnClass: 'btn-instagram',
+      faClass: 'fa-instagram',
+      tooltip: 'Follow me on Instagram'
+    },
+    {
+      platform: 'github',
+      link: 'https://github.com/khaneliman',
+      btnClass: 'btn-github',
+      faClass: 'fa-github',
+      tooltip: 'Star on GitHub'
+    },
+    {
+      platform: 'linkedin',
+      link: 'https://linkedin.com/in/austin-horstman-94963a76',
+      btnClass: 'btn-linkedin',
+      faClass: 'fa-linkedin',
+      tooltip: 'Star on linkedin'
+    }
+  ];
 
 }
