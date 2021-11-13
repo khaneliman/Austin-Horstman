218577b0568c322176722caad77d0cb52bff84d8
Austin Horstman <khaneliman12@gmail.com>
2021-11-13 16:08:00 -0600

    Update routing for new components and modules

--- WebApp/ClientApp/src/app/app-routing.module.ts
+++ WebApp/ClientApp/src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ContactComponent } from './personal/contact/contact.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
@@ -9,7 +8,7 @@ const routes: Routes = [
     path: 'personal',
     loadChildren: () => import('./personal/personal.module').then(m => m.PersonalModule)
   },
-  { path: '', redirectTo: 'home', pathMatch: 'full' }
+  { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
