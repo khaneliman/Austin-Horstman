ae5ab2484253707a2f33061663e8c14c6da11a43
Austin Horstman <khaneliman12@gmail.com>
2021-11-12 23:32:43 -0600

    Restructure into feature modules with feature routing. Create homepage.

--- WebApp/ClientApp/src/app/app-routing.module.ts
+++ WebApp/ClientApp/src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ResumeComponent } from './resume/resume.component';
-import { ContactComponent } from './contact/contact.component';
+import { ContactComponent } from './personal/contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: 'home', component: AboutComponent},
-  { path: 'about', component: AboutComponent},
-  { path: 'resume', component: ResumeComponent},
-  { path: 'contact', component: ContactComponent},
-  { path: '', redirectTo: '/home', pathMatch: 'full'}
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'personal',
+    loadChildren: () => import('./personal/personal.module').then(m => m.PersonalModule)
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
