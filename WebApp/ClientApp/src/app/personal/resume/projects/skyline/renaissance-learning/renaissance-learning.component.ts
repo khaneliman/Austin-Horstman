beda2012f285e885c7972e1f373b7d46d9d5af74
Austin Horstman <khaneliman12@gmail.com>
2021-11-16 14:12:50 -0600

    Projects pages updated
    add projects
    
    Fix broken test

---
+++ WebApp/ClientApp/src/app/personal/resume/projects/skyline/renaissance-learning/renaissance-learning.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-renaissance-learning',
+  templateUrl: './renaissance-learning.component.html',
+  styleUrls: ['./renaissance-learning.component.css']
+})
+export class RenaissanceLearningComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
