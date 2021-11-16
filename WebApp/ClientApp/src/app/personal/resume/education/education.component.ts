2c0611a2938131116500a0452a53d56e3be07e23
Austin Horstman <khaneliman12@gmail.com>
2021-11-15 20:54:47 -0600

    Employment restructuring
    Use route name for template styling on employment
    
    Add FVTC

--- WebApp/ClientApp/src/app/personal/resume/education/education.component.ts
+++ WebApp/ClientApp/src/app/personal/resume/education/education.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-education',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
+  public childActivated: Promise<string> | null = null;
+  private resolve: Function | null = null;
+  subscriptions: Subscription[] = []
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe())
   }
+  
+  ngOnInit(): void {
+    var jobs: string[] = [];
+    if (this.route.routeConfig) this.route.routeConfig.children.forEach(x => jobs.push(x.path));
+    
 
+    this.childActivated = new Promise<string>((resolve, reject) => {
+      this.resolve = resolve;
+    });
+
+    if (this.route.snapshot.firstChild) this.resolve(this.route.snapshot.firstChild.url[0].path);
+  }
 }
