67d345a805b35becc85ca981601765776d35d9a9
Austin Horstman <khaneliman12@gmail.com>
2021-11-15 20:54:47 -0600

    Employment restructuring
    Use route name for template styling on employment
    
    Add FVTC

--- WebApp/ClientApp/src/app/personal/resume/employment/employment-routing.module.ts
+++ WebApp/ClientApp/src/app/personal/resume/employment/employment-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { EmploymentComponent } from './employment.component';
 
 const routes: Routes = [
-  { path: '', component: EmploymentComponent },
-  { path: 'bestbuy', loadChildren: () => import('./bestbuy/bestbuy.module').then(m => m.BestbuyModule) },
-  { path: 'corebts', loadChildren: () => import('./corebts/corebts.module').then(m => m.CorebtsModule) },
-  { path: 'skyline', loadChildren: () => import('./skyline/skyline.module').then(m => m.SkylineModule) },
-  { path: 'west', loadChildren: () => import('./west/west.module').then(m => m.WestModule) },
+  {
+    path: '', component: EmploymentComponent, children: [
+      { path: 'bestbuy', loadChildren: () => import('./bestbuy/bestbuy.module').then(m => m.BestbuyModule) },
+      { path: 'corebts', loadChildren: () => import('./corebts/corebts.module').then(m => m.CorebtsModule) },
+      { path: 'skyline', loadChildren: () => import('./skyline/skyline.module').then(m => m.SkylineModule) },
+      { path: 'west', loadChildren: () => import('./west/west.module').then(m => m.WestModule) },
+    ]
+  },
+
   { path: '**', redirectTo: '', pathMatch: 'full' }];
 
 @NgModule({
--- WebApp/ClientApp/src/app/personal/resume/employment/employment.component.ts
+++ WebApp/ClientApp/src/app/personal/resume/employment/employment.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map, mergeMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-employment',
   templateUrl: './employment.component.html',
   styleUrls: ['./employment.component.css']
 })
-export class EmploymentComponent implements OnInit {
+export class EmploymentComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(private router: Router, private route: ActivatedRoute) { }
+  public childActivated: Promise<string> | null = null;
+  private resolve: Function | null = null;
+  subscriptions: Subscription[] = []
 
-  ngOnInit(): void {
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe())
   }
+  
+  ngOnInit(): void {
+    var jobs: string[] = [];
+    if (this.route.routeConfig) this.route.routeConfig.children.forEach(x => jobs.push(x.path));
+    
 
+    this.childActivated = new Promise<string>((resolve, reject) => {
+      this.resolve = resolve;
+    });
+
+    if (this.route.snapshot.firstChild) this.resolve(this.route.snapshot.firstChild.url[0].path);
+  }
 }
