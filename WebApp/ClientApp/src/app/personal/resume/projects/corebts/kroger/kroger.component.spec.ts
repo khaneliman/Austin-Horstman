9942ca9e9a863b9e89bd330a7a8387b53529f177
Austin Horstman <khaneliman12@gmail.com>
2021-11-16 14:12:50 -0600

    Projects pages updated
    add projects
    
    Fix broken test

---
+++ WebApp/ClientApp/src/app/personal/resume/projects/corebts/kroger/kroger.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KrogerComponent } from './kroger.component';
+
+describe('KrogerComponent', () => {
+  let component: KrogerComponent;
+  let fixture: ComponentFixture<KrogerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ KrogerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KrogerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
