c8c8e0160db613da8016f8555e0eecbf45e82483
Austin Horstman <khaneliman12@gmail.com>
2021-11-16 14:12:50 -0600

    Projects pages updated
    add projects
    
    Fix broken test

---
+++ WebApp/ClientApp/src/app/personal/resume/projects/skyline/jj-keller/jj-keller.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jj-keller',
+  templateUrl: './jj-keller.component.html',
+  styleUrls: ['./jj-keller.component.css']
+})
+export class JjKellerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
