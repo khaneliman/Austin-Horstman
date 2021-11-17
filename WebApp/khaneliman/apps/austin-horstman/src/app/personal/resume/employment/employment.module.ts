8b96158c4bfaf285615d28e3ebe6e85cb034c44e
Austin Horstman <khaneliman12@gmail.com>
2021-11-16 23:29:54 -0600

    NX Creation and port
    Verify working on garuda linux

---
+++ WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/employment/employment.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmploymentRoutingModule } from './employment-routing.module';
+import { EmploymentComponent } from './employment.component';
+
+import { SharedModule } from '../../../shared/shared.module';
+
+
+@NgModule({
+  declarations: [
+    EmploymentComponent,
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+    EmploymentRoutingModule
+  ]
+})
+export class EmploymentModule { }
