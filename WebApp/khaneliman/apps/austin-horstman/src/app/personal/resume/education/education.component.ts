7771c7e7114eca292ef117e6189ca16ce9c0f800
Austin Horstman <khaneliman12@gmail.com>
2021-11-16 23:29:54 -0600

    NX Creation and port
    Verify working on garuda linux

---
+++ WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/education/education.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent implements OnInit, OnDestroy {
+
+  constructor(private route: ActivatedRoute) { }
+  public childActivated: Promise<string> | null = null;
+  private resolve: Function | null = null;
+  subscriptions: Subscription[] = []
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe())
+  }
+
+  ngOnInit(): void {
+    const schools: string[] = [];
+    if (this.route && this.route.routeConfig && this.route.routeConfig.children) this.route.routeConfig.children.forEach(x => schools.push(x.path as string));
+
+    this.childActivated = new Promise<string>((resolve) => {
+      this.resolve = resolve;
+    });
+
+    if (this.route.snapshot.firstChild && this.resolve) this.resolve(this.route.snapshot.firstChild.url[0].path);
+  }
+}
