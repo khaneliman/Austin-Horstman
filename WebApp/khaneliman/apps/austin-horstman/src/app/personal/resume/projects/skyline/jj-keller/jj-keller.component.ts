77b562934d4fd0de945f378211b1d9f19e2e3c9a
Austin Horstman <khaneliman12@gmail.com>
2021-11-29 22:38:43 -0600

    Test Error Fixes

--- WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/projects/skyline/jj-keller/jj-keller.component.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/projects/skyline/jj-keller/jj-keller.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'khaneliman-jj-keller',
   templateUrl: './jj-keller.component.html',
   styleUrls: ['./jj-keller.component.css'],
 })
-export class JjKellerComponent implements OnInit {}
+export class JjKellerComponent {}
