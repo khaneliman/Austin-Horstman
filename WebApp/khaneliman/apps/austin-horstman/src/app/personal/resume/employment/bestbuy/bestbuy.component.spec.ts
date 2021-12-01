a4995c2e10800a2c753fa8f3656c1c3e90c04d65
Austin Horstman <khaneliman12@gmail.com>
2021-11-30 20:35:13 -0600

    Test fixes

--- WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/employment/bestbuy/bestbuy.component.spec.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/employment/bestbuy/bestbuy.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BestbuyComponent } from './bestbuy.component';
 
@@ -9,6 +10,7 @@ describe('BestbuyComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [BestbuyComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
--- WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/projects/geeksquad/geeksquad.component.spec.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/personal/resume/projects/geeksquad/geeksquad.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { GeeksquadComponent } from './geeksquad.component';
 
@@ -9,6 +10,7 @@ describe('GeeksquadComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [GeeksquadComponent],
+      imports: [RouterTestingModule],
     }).compileComponents();
   });
 
