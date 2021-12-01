a77e4595b9ca07eb03d9cd628d61e47c8d349f69
Austin Horstman <khaneliman12@gmail.com>
2021-11-30 19:21:33 -0600

    NPM scripts update

--- WebApp/khaneliman/apps/austin-horstman/src/app/app.module.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './khaneliman-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { SharedModule } from './shared/shared.module';
 import { HomeComponent } from './home/home.component';
