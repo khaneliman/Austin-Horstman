4f406e4029603e92af5ac65fbf93bf250b78956f
Austin Horstman <khaneliman12@gmail.com>
2022-04-20 21:17:13 -0500

    Fix tests

--- WebApp/khaneliman/apps/austin-horstman/src/app/shared/card/card.component.spec.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/shared/card/card.component.spec.ts
@@ -1,4 +1,7 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { SharedModule } from '../shared.module';
 
 import { CardComponent } from './card.component';
 
@@ -8,7 +11,10 @@ describe('CardComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CardComponent ]
+      declarations: [ CardComponent ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      // providers: [{ provide: MyService, useValue: {} }],
+      imports: [SharedModule, RouterTestingModule],
     })
     .compileComponents();
   });
