02f78183f7471195efd577b0e6089c149ca2a40e
Austin Horstman <khaneliman12@gmail.com>
2022-10-25 23:56:45 -0500

    Calculate age

--- WebApp/khaneliman/apps/austin-horstman/src/app/modules/personal/pages/about/about.component.ts
+++ WebApp/khaneliman/apps/austin-horstman/src/app/modules/personal/pages/about/about.component.ts
@@ -7,8 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AboutComponent {
   resumeShown = false;
+  birthdate = new Date('08/12/1991');
 
   showResume(): void {
     this.resumeShown = !this.resumeShown;
+  }
+
+  calculateAge(): number {
+    const timeDiff = Math.abs(Date.now() - this.birthdate.getTime());
+    return Math.floor(timeDiff / (1000 * 3600 * 24) / 365.25);
   }
 }
