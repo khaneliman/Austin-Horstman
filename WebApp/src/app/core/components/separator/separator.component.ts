2237ab128c50c6aad0115b058ac940116f3c49f3
Austin Horstman <khaneliman12@gmail.com>
2024-04-08 14:41:47 -0500

    core: move shared components to core

---
+++ WebApp/src/app/core/components/separator/separator.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-separator',
+  templateUrl: './separator.component.html',
+  styleUrls: ['./separator.component.scss'],
+})
+export class SeparatorComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
---
+++ WebApp/src/app/core/components/social-links/social-links.component.ts
@@ -0,0 +1,60 @@
+import { Component, Input } from '@angular/core';
+import { SocialLink } from './social-link';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true,
+  imports: [NgbTooltip, CommonModule],
+  selector: 'app-social-links',
+  templateUrl: './social-links.component.html',
+  styleUrls: ['./social-links.component.scss'],
+})
+export class SocialLinksComponent {
+  @Input() location: string | undefined;
+
+  socialLinks: SocialLink[] = [
+    {
+      platform: 'facebook',
+      link: 'https://facebook.com/khaneliman',
+      btnClass: 'btn-facebook',
+      faClass: 'fa-facebook-square',
+      tooltip: 'Follow me on Facebook',
+    },
+    {
+      platform: 'twitter',
+      link: 'https://twitter.com/khaneliman12',
+      btnClass: 'btn-twitter',
+      faClass: 'fa-twitter',
+      tooltip: 'Follow me on Twitter',
+    },
+    {
+      platform: 'instagram',
+      link: 'https://instagram.com/khaneliman',
+      btnClass: 'btn-instagram',
+      faClass: 'fa-instagram',
+      tooltip: 'Follow me on Instagram',
+    },
+    {
+      platform: 'github',
+      link: 'https://github.com/khaneliman',
+      btnClass: 'btn-github',
+      faClass: 'fa-github',
+      tooltip: 'Star on GitHub',
+    },
+    {
+      platform: 'gitlab',
+      link: 'https://gitlab.com/khaneliman',
+      btnClass: 'btn-gitlab',
+      faClass: 'fa-gitlab',
+      tooltip: 'Follow on GitLab',
+    },
+    {
+      platform: 'linkedin',
+      link: 'https://linkedin.com/in/austin-horstman-94963a76',
+      btnClass: 'btn-linkedin',
+      faClass: 'fa-linkedin',
+      tooltip: 'Connect on LinkedIn',
+    },
+  ];
+}
