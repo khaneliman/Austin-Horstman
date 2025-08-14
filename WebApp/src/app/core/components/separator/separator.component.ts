dd35bbc4bff1bcc07cacdefc1ed185d91a7c4f62
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 20:20:14 -0500

    perf(separator): add OnPush change detection strategy
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- WebApp/src/app/core/components/separator/separator.component.ts
+++ WebApp/src/app/core/components/separator/separator.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   standalone: true,
   selector: 'app-separator',
   templateUrl: './separator.component.html',
   styleUrls: ['./separator.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SeparatorComponent {}
