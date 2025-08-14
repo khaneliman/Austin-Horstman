005f78c1911a829cde67cb0407146f71b2bc831d
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 20:20:32 -0500

    refactor: add barrel export for core components
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

---
+++ WebApp/src/app/core/components/index.ts
@@ -0,0 +1,6 @@
+export { CardComponent } from './card/card.component';
+export { FloatingCardComponent } from './floating-card/floating-card.component';
+export { FooterComponent } from './footer/footer.component';
+export { NavbarComponent } from './navbar/navbar.component';
+export { SeparatorComponent } from './separator/separator.component';
+export { SocialLinksComponent } from './social-links/social-links.component';
