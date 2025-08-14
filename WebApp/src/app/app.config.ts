55f7b4fed7f7a837c62468a2946a0b42e3432152
Austin Horstman <khaneliman12@gmail.com>
2025-08-14 01:34:27 -0500

    feat: add ng-icons with heroicons and simple-icons
    - Install @ng-icons/core, @ng-icons/heroicons-outline, @ng-icons/simple-icons
    - Configure global ng-icons settings in app.config.ts

--- WebApp/src/app/app.config.ts
+++ WebApp/src/app/app.config.ts
@@ -1,8 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideNgIconsConfig } from '@ng-icons/core';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)],
+  providers: [
+    provideRouter(routes),
+    provideNgIconsConfig({
+      size: '1.5em',
+      color: 'currentColor',
+    }),
+  ],
 };
