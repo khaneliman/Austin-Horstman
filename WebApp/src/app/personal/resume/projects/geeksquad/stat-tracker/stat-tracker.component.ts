642715f95e2fb4d7dc9a8a94a6396f09b94f4e53
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 23:00:15 -0500

    refactor: replace breadcrumbs with modern navigation in Geek Squad project

--- WebApp/src/app/personal/resume/projects/geeksquad/stat-tracker/stat-tracker.component.ts
+++ WebApp/src/app/personal/resume/projects/geeksquad/stat-tracker/stat-tracker.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-stat-tracker',
   templateUrl: './stat-tracker.component.html',
   styleUrls: ['./stat-tracker.component.scss'],
+  imports: [RouterLink],
 })
 export class StatTrackerComponent {}
