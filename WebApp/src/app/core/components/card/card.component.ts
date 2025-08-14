c613239f0e8f3a717411ab0f1da59b9be587244e
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 20:20:04 -0500

    perf(card): add OnPush change detection strategy
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- WebApp/src/app/core/components/card/card.component.ts
+++ WebApp/src/app/core/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -7,6 +7,7 @@ import { RouterModule } from '@angular/router';
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
   @Input() imgSrc: string | undefined;
