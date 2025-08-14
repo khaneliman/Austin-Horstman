f9de40342b76e6fe475f117b1acdaad1ede9c44d
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 22:59:43 -0500

    refactor: redesign Core BTS company page to focused project hub

--- WebApp/src/app/personal/resume/projects/corebts/kroger/kroger.component.ts
+++ WebApp/src/app/personal/resume/projects/corebts/kroger/kroger.component.ts
@@ -1,21 +1,11 @@
 import { Component } from '@angular/core';
-import {
-  BreadcrumbComponent,
-  BreadcrumbItem,
-} from '../../../../../shared/components/breadcrumb/breadcrumb.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-kroger',
   templateUrl: './kroger.component.html',
   styleUrls: ['./kroger.component.scss'],
   standalone: true,
-  imports: [BreadcrumbComponent],
+  imports: [RouterLink],
 })
-export class KrogerComponent {
-  breadcrumbItems: BreadcrumbItem[] = [
-    { label: 'Projects', route: '/projects' },
-    { label: 'Professional', route: '/projects/professional' },
-    { label: 'Core BTS', route: '/projects/professional/corebts' },
-    { label: 'Kroger Solutions', active: true },
-  ];
-}
+export class KrogerComponent {}
