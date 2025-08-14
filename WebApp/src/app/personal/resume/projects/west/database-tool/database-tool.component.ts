f54cbf60f557663b8bbcc9f2150fb38143bd15ae
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 23:00:05 -0500

    refactor: replace breadcrumbs with modern navigation in West projects

--- WebApp/src/app/personal/resume/projects/west/database-tool/database-tool.component.ts
+++ WebApp/src/app/personal/resume/projects/west/database-tool/database-tool.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-database-tool',
   templateUrl: './database-tool.component.html',
   styleUrls: ['./database-tool.component.scss'],
+  imports: [RouterLink],
 })
 export class DatabaseToolComponent {}
--- WebApp/src/app/personal/resume/projects/west/it-portal/it-portal.component.ts
+++ WebApp/src/app/personal/resume/projects/west/it-portal/it-portal.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-it-portal',
   templateUrl: './it-portal.component.html',
   styleUrls: ['./it-portal.component.scss'],
+  imports: [RouterLink],
 })
 export class ItPortalComponent {}
