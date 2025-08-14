8c5a37fedce23c531a7b7dfafd2ac3ee7b2130ad
Austin Horstman <khaneliman12@gmail.com>
2025-08-13 20:20:28 -0500

    perf(navbar): add OnPush change detection and proper subscription cleanup
    Signed-off-by: Austin Horstman <khaneliman12@gmail.com>

--- WebApp/src/app/core/components/navbar/navbar.component.ts
+++ WebApp/src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy, OnInit, inject } from '@angular/core';
 import {
   Router,
   NavigationEnd,
@@ -8,37 +8,48 @@ import {
 import { Location, PopStateEvent } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SocialLinksComponent } from '../social-links/social-links.component';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   imports: [NgbModule, RouterModule, SocialLinksComponent],
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent implements OnInit, OnDestroy {
   public isCollapsed = true;
   private lastPoppedUrl: string | undefined;
   private yScrollStack: number[] = [];
+  private destroy$ = new Subject<void>();
 
   public location = inject(Location);
   private router = inject(Router);
 
   ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      this.isCollapsed = true;
-      if (event instanceof NavigationStart) {
-        if (event.url != this.lastPoppedUrl)
-          this.yScrollStack.push(window.scrollY);
-      } else if (event instanceof NavigationEnd) {
-        if (event.url == this.lastPoppedUrl) {
-          this.lastPoppedUrl = undefined;
-          window.scrollTo(0, this.yScrollStack.pop() as number);
-        } else window.scrollTo(0, 0);
-      }
-    });
+    this.router.events
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(event => {
+        this.isCollapsed = true;
+        if (event instanceof NavigationStart) {
+          if (event.url != this.lastPoppedUrl)
+            this.yScrollStack.push(window.scrollY);
+        } else if (event instanceof NavigationEnd) {
+          if (event.url == this.lastPoppedUrl) {
+            this.lastPoppedUrl = undefined;
+            window.scrollTo(0, this.yScrollStack.pop() as number);
+          } else window.scrollTo(0, 0);
+        }
+      });
+    
     this.location.subscribe((ev: PopStateEvent) => {
       this.lastPoppedUrl = ev.url;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 
   isHome(): boolean {
