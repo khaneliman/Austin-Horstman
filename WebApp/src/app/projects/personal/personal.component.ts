e888f430649482b2e79715bc493361cf98e8f9cd
Austin Horstman <khaneliman12@gmail.com>
2025-08-14 19:49:28 -0500

    feat: integrate personal projects grid in dedicated page
    - Replace placeholder content with PersonalProjectsGridComponent
    - Import and use generatePersonalProjectsGrid for data
    - Add required icon imports for grid component
    - Display all personal projects with full details

--- WebApp/src/app/projects/personal/personal.component.ts
+++ WebApp/src/app/projects/personal/personal.component.ts
@@ -11,13 +11,23 @@ import {
   heroBriefcase,
   heroComputerDesktop,
   heroArrowTopRightOnSquare,
+  heroCheckCircle,
+  heroWrench,
+  heroDevicePhoneMobile,
 } from '@ng-icons/heroicons/outline';
+import { PersonalProjectsGridComponent } from '../../shared/components/personal-projects-grid/personal-projects-grid.component';
+import { generatePersonalProjectsGrid } from '../../shared/data/personal-projects';
 
 @Component({
   selector: 'app-personal',
   templateUrl: './personal.component.html',
   styleUrls: ['./personal.component.scss'],
-  imports: [RouterOutlet, RouterLink, NgIconComponent],
+  imports: [
+    RouterOutlet,
+    RouterLink,
+    NgIconComponent,
+    PersonalProjectsGridComponent,
+  ],
   providers: [
     provideIcons({
       heroUser,
@@ -29,7 +39,12 @@ import {
       heroBriefcase,
       heroComputerDesktop,
       heroArrowTopRightOnSquare,
+      heroCheckCircle,
+      heroWrench,
+      heroDevicePhoneMobile,
     }),
   ],
 })
-export class PersonalComponent {}
+export class PersonalComponent {
+  personalProjects = generatePersonalProjectsGrid();
+}
