6488bc0e1716bf1088147a903e781aa1c91a6530
Austin Horstman <khaneliman12@gmail.com>
2025-08-15 13:24:53 -0500

    feat: create reusable project detail template component

---
+++ WebApp/src/app/shared/components/project-detail-template/project-detail-template.component.ts
@@ -0,0 +1,73 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import {
+  heroArrowLeft,
+  heroBookOpen,
+  heroRocketLaunch,
+  heroCircleStack,
+  heroCodeBracket,
+  heroComputerDesktop,
+  heroCog6Tooth,
+  heroLightBulb,
+  heroChartBar,
+  heroGlobeAlt,
+  heroShieldCheck,
+  heroBeaker,
+  heroWrench,
+  heroAcademicCap,
+  heroUserGroup,
+  heroCloudArrowUp,
+  heroDocumentText,
+  heroMagnifyingGlass,
+  heroSparkles,
+  heroServer,
+  heroDevicePhoneMobile,
+  heroClock,
+  heroUser,
+  heroBolt,
+  heroWifi,
+} from '@ng-icons/heroicons/outline';
+
+import { ProjectDetailConfig } from '../../interfaces/project-detail.interface';
+import { ProjectNavHeaderComponent } from '../project-nav-header/project-nav-header.component';
+
+@Component({
+  selector: 'app-project-detail-template',
+  standalone: true,
+  imports: [CommonModule, NgIconComponent, ProjectNavHeaderComponent],
+  providers: [
+    provideIcons({
+      heroArrowLeft,
+      heroBookOpen,
+      heroRocketLaunch,
+      heroCircleStack,
+      heroCodeBracket,
+      heroComputerDesktop,
+      heroCog6Tooth,
+      heroLightBulb,
+      heroChartBar,
+      heroGlobeAlt,
+      heroShieldCheck,
+      heroBeaker,
+      heroWrench,
+      heroAcademicCap,
+      heroUserGroup,
+      heroCloudArrowUp,
+      heroDocumentText,
+      heroMagnifyingGlass,
+      heroSparkles,
+      heroServer,
+      heroDevicePhoneMobile,
+      heroClock,
+      heroUser,
+      heroBolt,
+      heroWifi,
+    }),
+  ],
+  templateUrl: './project-detail-template.component.html',
+  styleUrl: './project-detail-template.component.scss',
+})
+export class ProjectDetailTemplateComponent {
+  @Input({ required: true }) config!: ProjectDetailConfig;
+}
