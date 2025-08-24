15520f62432ac29ca43708ddb7db0e877a460300
Austin Horstman <khaneliman12@gmail.com>
2025-08-24 16:28:09 -0500

    fix: wave-separator was invisible on resume

--- WebApp/src/app/shared/components/wave-separator/wave-separator.component.ts
+++ WebApp/src/app/shared/components/wave-separator/wave-separator.component.ts
@@ -15,10 +15,10 @@ export type WaveDirection = 'top' | 'bottom' | 'both';
         class="relative block w-full"
         [attr.height]="svgHeight"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1200 120"
+        viewBox="0 0 1440 320"
         preserveAspectRatio="none"
       >
-        <path [attr.d]="getPathData('top')" [class]="getPathClasses('top')"></path>
+        <path [attr.d]="getPathData('top')" [class]="getPathClasses('top')" [ngStyle]="getPathStyle('top')"></path>
       </svg>
     </div>
 
@@ -31,10 +31,14 @@ export type WaveDirection = 'top' | 'bottom' | 'both';
         class="relative block w-full"
         [attr.height]="svgHeight"
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1200 120"
+        viewBox="0 0 1440 320"
         preserveAspectRatio="none"
       >
-        <path [attr.d]="getPathData('bottom')" [class]="getPathClasses('bottom')"></path>
+        <path
+          [attr.d]="getPathData('bottom')"
+          [class]="getPathClasses('bottom')"
+          [ngStyle]="getPathStyle('bottom')"
+        ></path>
       </svg>
     </div>
   `,
@@ -73,7 +77,7 @@ export class WaveSeparatorComponent {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   getPathClasses(position: 'top' | 'bottom'): string {
-    const classes = [`fill-${this.color}`];
+    const classes: string[] = [];
 
     if (this.opacity !== 100) {
       classes.push(`opacity-${this.opacity}`);
@@ -86,6 +90,13 @@ export class WaveSeparatorComponent {
     return classes.join(' ');
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  getPathStyle(position: 'top' | 'bottom'): Record<string, string> {
+    return {
+      fill: this.color,
+    };
+  }
+
   getPathData(position: 'top' | 'bottom'): string {
     const baseFlip = this.flip;
     const positionFlip = position === 'top';
@@ -93,15 +104,15 @@ export class WaveSeparatorComponent {
 
     switch (this.waveStyle) {
       case 'wave':
-        return 'M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z';
+        return 'M0,64L48,80C96,96,192,128,288,133.3C384,139,480,117,576,106.7C672,96,768,96,864,112C960,128,1056,160,1152,160C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z';
 
       case 'curve':
         return shouldFlip ? 'M0,160L1440,96L1440,0L0,0Z' : 'M0,96L1440,160L1440,320L0,320Z';
 
       case 'zigzag':
         return shouldFlip
-          ? 'M0,128L120,96L240,128L360,64L480,128L600,96L720,128L840,64L960,128L1080,96L1200,128L1200,0L0,0Z'
-          : 'M0,64L120,96L240,64L360,128L480,64L600,96L720,64L840,128L960,64L1080,96L1200,64L1200,320L0,320Z';
+          ? 'M0,128L120,96L240,128L360,64L480,128L600,96L720,128L840,64L960,128L1080,96L1200,128L1440,96L1440,0L0,0Z'
+          : 'M0,64L120,96L240,64L360,128L480,64L600,96L720,64L840,128L960,64L1080,96L1200,64L1440,96L1440,320L0,320Z';
 
       case 'slant':
         return shouldFlip ? 'M0,320L1440,160L1440,0L0,0Z' : 'M0,160L1440,320L1440,320L0,320Z';
@@ -111,8 +122,8 @@ export class WaveSeparatorComponent {
 
       case 'book':
         return shouldFlip
-          ? 'M0,160L240,96L480,160L720,64L960,160L1200,96L1200,0L0,0Z'
-          : 'M0,96L240,160L480,96L720,160L960,96L1200,160L1200,320L0,320Z';
+          ? 'M0,160L240,96L480,160L720,64L960,160L1200,96L1440,128L1440,0L0,0Z'
+          : 'M0,96L240,160L480,96L720,160L960,96L1200,160L1440,128L1440,320L0,320Z';
 
       default:
         return 'M0,64L48,80C96,96,192,128,288,133.3C384,139,480,117,576,106.7C672,96,768,96,864,112C960,128,1056,160,1152,160C1248,160,1344,128,1392,112L1440,96L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z';
