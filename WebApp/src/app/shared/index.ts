41e18f0cfc78203e6aa9ea71a7dfbb9b945a557d
Austin Horstman <khaneliman12@gmail.com>
2026-05-27 22:23:08 -0500

    chore: remove unused shared components

--- WebApp/src/app/shared/index.ts
+++ WebApp/src/app/shared/index.ts
@@ -1,4 +1,3 @@
 export * from './components/enhanced-feature-card/enhanced-feature-card.component';
-export * from './components/project-card/project-card.component';
 export * from './components/project-nav-header/project-nav-header.component';
 export * from './types';
